b8f6c1feb368dac79cc559f9105c41c575b388c3
ikepon <tatsuyanoheya@gmail.com>
2024-10-06 12:34:04 +0900

    feat :Get who from Body and Header

--- hello.py
+++ hello.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Body, Header
 
 app = FastAPI()
 
@@ -8,3 +8,13 @@ def greet(
     who: str, word: str
 ):  # path function, こっちだけに定義するとクエリパラメータになる
     return f"{word} {who}"
+
+
+@app.post("/hi")
+def greet_post(who: str = Body(embed=True)):
+    return f"hello {who}: body"
+
+
+@app.post("/hi/header")
+def greet_query(who: str = Header()):
+    return f"hello {who}: header"
