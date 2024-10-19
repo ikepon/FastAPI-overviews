fe174a846b6be016b0030be7220e49273a9a24be
ikepon <tatsuyanoheya@gmail.com>
2024-10-20 08:49:42 +0900

    feat: Basic認証

--- src/web/auth.py
+++ src/web/auth.py
@@ -1,11 +1,16 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from fastapi.security import HTTPBasic, HTTPBasicCredentials
 
-router = APIRouter
+router = APIRouter()
+
+secret_user: str = "newphone"
+secret_password: str = "whodis"
 
 basic = HTTPBasic()
 
 
 @router.get("/who")
-def get_user(creds: HTTPBasicCredentials = Depends(basic)):
-    return {"username": creds.username, "password": creds.password}
+def get_user(creds: HTTPBasicCredentials = Depends(basic)) -> dict:
+    if creds.username == secret_user and creds.password == secret_password:
+        return {"username": creds.username, "password": creds.password}
+    raise HTTPException(status_code=401, detail="Hey!")
